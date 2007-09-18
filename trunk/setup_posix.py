4133268fc83bd174472d42188663b1b3679e8f4f
harry.python <harry.python@eda37374-573a-0410-b0c9-7b4335ea722e>
2007-09-18 06:42:14 +0000

    参考MySQLdb的方式修改了结构，使用一个通用的setup.py来统筹管理win32/linux的安装。

---
+++ trunk/setup_posix.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: linux/setup.py
+# Date: 2007-09-18
+# Author: gashero
+# Copyright@1999-2007, Harry Gashero Liu.
+
+"""
+构建脚本
+"""
+
+from distutils.core import setup,Extension
+
+module_exlibnet=Extension('sendpkt',
+        sources=['exlibnet.c',],
+        libraries=['net',]
+        )
+
+setup(name="sendpkt",
+        version="0.1",
+        description="Send ethernet frame in Linux or Win32",
+        ext_modules=[module_exlibnet,],
+        author="gashero",
+        author_email="harry.python@gmail.com",
+        url="http://gashero.yeax.com/"
+        )
