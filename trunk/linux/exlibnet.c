10e3cbdebbdc59b57ffce8d2f7ca01e8d5e56df0
harry.python <harry.python@eda37374-573a-0410-b0c9-7b4335ea722e>
2007-09-18 06:25:21 +0000

    加入了单元测试模块，尝试传递关键字参数时一直碰壁，说是段错误。

--- trunk/linux/exlibnet.c
+++ trunk/linux/exlibnet.c
@@ -8,21 +8,30 @@
 #include <Python.h>
 #include <libnet.h>
 
+//----------------------实体函数--------------------------------------------
+
+unsigned int exlibnet_sendpacket(unsigned char* packet) {
+    return 0;
+}
+
+//----------------------Python接口函数--------------------------------------
+
 static PyObject* sendpacket(PyObject* self, PyObject* args, PyObject* keywds) {
     PyObject* packet=NULL;
+    printf("hello");
     static char* kwlist[]={"packet",NULL};
     if (!PyArg_ParseTupleAndKeywords(args,keywds,"O",kwlist,packet)) {
         return NULL;
     }
-    printf("Address: 0x%08x\n",(unsigned long)packet);
+    printf("Address: 0x%08x\n",(unsigned int)packet);
     Py_RETURN_NONE;
-};
+}
 
 static PyMethodDef SendPktMethods[]={
-    {"sendpacket",sendpacket,METH_VARARGS|METH_KEYWORDS,""},
+    {"sendpacket",(PyCFunction)sendpacket,METH_VARARGS|METH_KEYWORDS,"Send a packet to ethernet"},
     {NULL,NULL,0,NULL}
 };
 
 PyMODINIT_FUNC initsendpkt(void) {
     Py_InitModule("sendpkt",SendPktMethods);
-};
+}
