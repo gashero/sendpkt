0b00fa5277a176871404b044f057f30c46af63a9
harry.python <harry.python@eda37374-573a-0410-b0c9-7b4335ea722e>
2007-09-18 06:49:08 +0000

    新加了Win32下的C文件exwinpcap.c，并且基本制作了相应的win32下的构建脚本。

---
+++ setup_nt.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+# File: setup.py
+# Date: 2007-09-18
+# Author: gashero
+# Copyright@1999-2007, Harry Gashero Liu.
+
+"""
+win32构建脚本
+"""
+
+from distutils.core import setup,Extension
+
+module_exwinpcap=Extension('sendpkt',
+        sources=['exwinpcap.c',],
+        libraries=['wpcap',]
+        )
+
+setup(name="sendpkt",
+        version="0.1",
+        description="Send ethernet frame in Linux or Win32",
+        ext_modules=[module_exwinpcap,],
+        author="gashero",
+        author_email="harry.python@gmail.com",
+        url="http://gashero.yeax.com/"
+        )
