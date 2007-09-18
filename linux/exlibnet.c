50814a7e896dd812f8ce67fe81e77bbc99a203c7
harry.python <harry.python@eda37374-573a-0410-b0c9-7b4335ea722e>
2007-09-18 06:01:02 +0000

    基本完成了exlibnet.c中的程序框架了。

--- linux/exlibnet.c
+++ linux/exlibnet.c
@@ -7,3 +7,22 @@
 
 #include <Python.h>
 #include <libnet.h>
+
+static PyObject* sendpacket(PyObject* self, PyObject* args, PyObject* keywds) {
+    PyObject* packet=NULL;
+    static char* kwlist[]={"packet",NULL};
+    if (!PyArg_ParseTupleAndKeywords(args,keywds,"O",kwlist,packet)) {
+        return NULL;
+    }
+    printf("Address: 0x%08x\n",(unsigned long)packet);
+    Py_RETURN_NONE;
+};
+
+static PyMethodDef SendPktMethods[]={
+    {"sendpacket",sendpacket,METH_VARARGS|METH_KEYWORDS,""},
+    {NULL,NULL,0,NULL}
+};
+
+PyMODINIT_FUNC initsendpkt(void) {
+    Py_InitModule("sendpkt",SendPktMethods);
+};
