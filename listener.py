1354f9d114d365a4847ac2bdc394041c61a67aaf
harry.python <harry.python@eda37374-573a-0410-b0c9-7b4335ea722e>
2007-09-18 08:23:48 +0000

    测试发包成功，好酷！！！

--- listener.py
+++ listener.py
@@ -4,3 +4,24 @@
 # Date: 2007-09-18
 # Author: gashero
 # Copyright@1999-2007, Harry Gashero Liu.
+
+import time
+
+import pcap
+
+DATETIME_FORMAT="%Y-%m-%d %H:%M:%S"
+PCAP_FILTER=""
+
+def main():
+    pc=pcap.pcap()
+    pc.setfilter(PCAP_FILTER)
+    try:
+        for ptime,pdata in pc:
+            print time.strftime(DATETIME_FORMAT)
+            print repr(str(pdata))
+    except KeyboardInterrupt:
+        print "EXIT"
+    return
+
+if __name__=="__main__":
+    main()
